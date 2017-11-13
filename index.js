aecd3fda31468386e48f59f98a3e896f0548264b
kent winter <kgw700@gmail.com>
2017-11-13 03:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-/function happyHolidays() {
+var name = "Kent"
+var holiday = "Thanksgiving"
+var days = 14
+function happyHolidays() {
   return 'Happy holidays!'
 }
-function happyHolidaysTo(you) {
-  return `Happy holidays, ${you}!`
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function happyCountdown(holiday, days) {
+  return `Happy ${holiday}, ${days}!`
 }
