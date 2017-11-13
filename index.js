910ca4911b0ee05a9b5f64cce835784468143409
kent winter <kgw700@gmail.com>
2017-11-13 03:23:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days) {
-  return `Happy ${holiday}, ${days}!`
+  return `It's ${days} days until ${holiday}!`
 }
+
+  
